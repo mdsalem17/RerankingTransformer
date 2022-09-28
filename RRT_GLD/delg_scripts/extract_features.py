e64be2c474e1ee190cd172b406f7a1ee73049918
Salem Messoud <mmsalem78@gmail.com>
2022-09-28 09:07:51 +0200

    add scripts to extract ViQuAE features

--- RRT_GLD/delg_scripts/extract_features.py
+++ RRT_GLD/delg_scripts/extract_features.py
@@ -1,3 +1,29 @@
+# Lint as: python3
+# Copyright 2020 The TensorFlow Authors All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Extracts DELG features for images from Revisited Oxford/Paris datasets.
+
+Note that query images are cropped before feature extraction, as required by the
+evaluation protocols of these datasets.
+
+The types of extracted features (local and/or global) depend on the input
+DelfConfig.
+
+The program checks if features already exist, and skips computation for those.
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -15,7 +41,7 @@ from delf import delf_config_pb2
 from delf import datum_io
 from delf import feature_io
 from delf import utils
-from delf.python.detect_to_retrieve import dataset
+from delf.python.datasets.revisited_op import dataset
 from delf import extractor
 
 FLAGS = flags.FLAGS
@@ -31,6 +57,8 @@ flags.DEFINE_string(
 flags.DEFINE_string(
     'images_dir', '/tmp/images',
     'Directory where dataset images are located, all in .jpg format.')
+flags.DEFINE_enum('image_set', 'query', ['query', 'index'],
+                  'Whether to extract features from query or index images.')
 flags.DEFINE_string(
     'output_features_dir', '/tmp/features',
     "Directory where DELG features will be written to. Each image's features "
@@ -41,15 +69,10 @@ flags.DEFINE_string(
 # Extensions.
 _DELG_GLOBAL_EXTENSION = '.delg_global'
 _DELG_LOCAL_EXTENSION = '.delg_local'
+_IMAGE_EXTENSION = '.jpg'
 
 # Pace to report extraction log.
-_STATUS_CHECK_ITERATIONS = 100
-	
-
-def read_file(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-    return lines
+_STATUS_CHECK_ITERATIONS = 50
 
 
 def main(argv):
@@ -58,7 +81,12 @@ def main(argv):
 
   # Read list of images from dataset file.
   print('Reading list of images from dataset file...')
-  image_list = read_file(FLAGS.dataset_file_path)
+  query_list, index_list, ground_truth = dataset.ReadDatasetFile(
+      FLAGS.dataset_file_path)
+  if FLAGS.image_set == 'query':
+    image_list = query_list
+  else:
+    image_list = index_list
   num_images = len(image_list)
   print('done! Found %d images' % num_images)
 
@@ -74,7 +102,6 @@ def main(argv):
   extractor_fn = extractor.MakeExtractor(config)
 
   start = time.time()
-  missing_images = []
   for i in range(num_images):
     if i == 0:
       print('Starting to extract features...')
@@ -84,23 +111,22 @@ def main(argv):
             'images took %f seconds' %
             (i, num_images, _STATUS_CHECK_ITERATIONS, elapsed))
       start = time.time()
-    line = image_list[i]
-    image_path, image_label, image_width, image_height = line.split(';;')
-    image_name = os.path.splitext(os.path.basename(image_path))[0]
-    input_image_filename = os.path.join(FLAGS.images_dir, image_path)
-    if not os.path.exists(input_image_filename):
-        missing_images.append(image_name)
-        continue
-    
+
+    image_name = image_list[i]
+    input_image_filename = os.path.join(FLAGS.images_dir,
+                                        image_name + _IMAGE_EXTENSION)
+
     # Compose output file name and decide if image should be skipped.
     should_skip_global = True
     should_skip_local = True
     if config.use_global_features:
-      output_global_feature_filename = os.path.join(FLAGS.output_features_dir, image_name + _DELG_GLOBAL_EXTENSION)
+      output_global_feature_filename = os.path.join(
+          FLAGS.output_features_dir, image_name + _DELG_GLOBAL_EXTENSION)
       if not tf.io.gfile.exists(output_global_feature_filename):
         should_skip_global = False
     if config.use_local_features:
-      output_local_feature_filename = os.path.join(FLAGS.output_features_dir, image_name + _DELG_LOCAL_EXTENSION)
+      output_local_feature_filename = os.path.join(
+          FLAGS.output_features_dir, image_name + _DELG_LOCAL_EXTENSION)
       if not tf.io.gfile.exists(output_local_feature_filename):
         should_skip_local = False
     if should_skip_global and should_skip_local:
@@ -109,13 +135,14 @@ def main(argv):
 
     pil_im = utils.RgbLoader(input_image_filename)
     resize_factor = 1.0
-    # if FLAGS.image_set == 'query':
-    #   # Crop query image according to bounding box.
-    #   original_image_size = max(pil_im.size)
-    #   bbox = [int(round(b)) for b in ground_truth[i]['bbx']]
-    #   pil_im = pil_im.crop(bbox)
-    #   cropped_image_size = max(pil_im.size)
-    #   resize_factor = cropped_image_size / original_image_size
+    if FLAGS.image_set == 'query':
+      # Crop query image according to bounding box.
+      original_image_size = max(pil_im.size)
+      bbox = [int(round(b)) for b in ground_truth[i]['bbx']]
+      pil_im = pil_im.crop(bbox)
+      cropped_image_size = max(pil_im.size)
+      resize_factor = cropped_image_size / original_image_size
+
     im = np.array(pil_im)
 
     # Extract and save features.
@@ -130,10 +157,6 @@ def main(argv):
       attention = extracted_features['local_features']['attention']
       feature_io.WriteToFile(output_local_feature_filename, locations,
                              feature_scales, descriptors, attention)
-
-  print('count', len(missing_images))
-  with open('missing.txt', 'w') as f:
-    f.write('\n'.join(missing_images))
 
 
 if __name__ == '__main__':
